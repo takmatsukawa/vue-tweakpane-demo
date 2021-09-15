8531e88153bacaf3022c53a477d4ddb6c69d0fea
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-09-15 21:16:26 +0900

    Folder

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,13 @@ const params = ref({
 
 <template>
   <TPane>
-    <TInput v-model="params.factor" name="factor"></TInput>
-    <TInput v-model="params.title" name="title"></TInput>
-    <TInput v-model="params.color" name="color"></TInput>
+    <TFolder title="Group1">
+      <TInput v-model="params.factor" name="factor"></TInput>
+      <TInput v-model="params.title" name="title"></TInput>
+      <TInput v-model="params.color" name="color"></TInput>
+    </TFolder>
 
-    <TFolder title="Folder">
+    <TFolder title="Group2">
       <TInput v-model="params.percentage" name="percentage" :opt-params="{min: 0, max: 100, step: 1}"></TInput>
       <TInput v-model="params.theme" name="theme" :opt-params="{options: {Dark: 'dark', Light: 'light'}}"></TInput>
     </TFolder>
